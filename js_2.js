c0083f308a0881a1b624143ce4bb422b81a6f223
Nadin <nad16-92@mail.ru>
2018-02-02 18:57:04 +0300

    pervii2

--- js_2.js
+++ js_2.js
@@ -3,8 +3,8 @@ a=0.2;
 b=2;
 if((0.5<(a*b))&&((a*b)<10)){
     c=Math.exp((Math.abs(a))-(Math.abs(b)));
-} else if ((0.1 < (a * b)) && ((a * b) < 0.5)) {
+} else if ((0.6 < (a * b)) && ((a * b) < 0.5)) {
     c = Math.sqrt(Math.abs(a + b));
 }
-else c = (b * b) * 2;
+else c = (b * b) * 3;
 console.log(c);
