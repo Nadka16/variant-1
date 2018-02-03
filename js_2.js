c625f36c9f33a78ca76cb39cf2b1af61dc809ca7
Nadin <nad16-92@mail.ru>
2018-02-03 14:48:35 +0300

    tri

--- js_2.js
+++ js_2.js
@@ -3,8 +3,7 @@ a=0.2;
 b=2;
 if((0.3<(a*b))&&((a*b)<10)){
     c=Math.exp((Math.abs(a))-(Math.abs(b)));
-} else if ((0.1 < (a * b)) && ((a * b) < 0.3)) {
-    c = Math.sqrt(Math.abs(a + b));
+} 
 }
 else c = (b * b) * 3;
 console.log(c);
